2b7fe3c432e7739556cf84c1d320d1c3e4bd8144
dgop92 <inevaup@gmail.com>
2021-08-16 11:12:06 -0500

    initial commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import game.GameSketch;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        GameSketch gameSketch = new GameSketch();
+        gameSketch.run();
+    }
+}
